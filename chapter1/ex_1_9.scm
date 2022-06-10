2dbff2fb7caf8750ed9801e4b8fd207f66a813c1
Marcin Sztylko <marcin.sztylko@gmail.com>
2022-06-10 10:23:58 +0200

    add explanation for ex1.9

--- chapter1/ex_1_9.scm
+++ chapter1/ex_1_9.scm
@@ -4,16 +4,23 @@
 (define (dec x)
     (- x 1))
 
+;; recursive prodecure - calls itself
+;; recursive process - call to itself in a nested scope
+;; doesn't get evaluate but rather creates deferred chain of operations
 (define (add1 a b)
     (if (= a 0)
         b
-        (inc (+ (dec a) b))))
-
-(define (add2 a b)
-    (if (= a 0)
-        b
-        (+ (dec a) (inc b))))
+        (inc (add1 (dec a) b))))
 
 (display (add1 4 5))
 (display "\n")
+
+;; recursive prodecure - calls itself
+;; iterative process - calls itself at the top level so arguments
+;; are first evaluated 
+(define (add2 a b)
+    (if (= a 0)
+        b
+        (add2 (dec a) (inc b))))
+
 (display (add2 4 5))
