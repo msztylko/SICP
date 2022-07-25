c5952f67a960712db7eaa9cc6766f39ae68059b9
Marcin Sztylko <marcin.sztylko@gmail.com>
2022-07-25 10:15:20 +0200

    add ex2.61

--- chapter2/ch2.3/set_ordered_list.scm
+++ chapter2/ch2.3/set_ordered_list.scm
@@ -17,6 +17,22 @@
 (newline)
 (display (adjoint-set 2 `(1 3 4)))
 
-;;(define (intersection-set set1 set2)
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()    
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+(newline)
+(display (intersection-set `(1 2 3) `(1 2 3)))
+(newline)
+(display (intersection-set `(1 4 5 7 8) `(2 3 5 6 7)))
 
 ;;(define (union-set set1 set2)
