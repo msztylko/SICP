95c3a807de083f12a414ebea20d623b21c083ba4
Marcin Sztylko <marcin.sztylko@gmail.com>
2022-07-25 10:23:53 +0200

    add union-set

--- chapter2/ch2.3/set_ordered_list.scm
+++ chapter2/ch2.3/set_ordered_list.scm
@@ -35,4 +35,13 @@
 (newline)
 (display (intersection-set `(1 4 5 7 8) `(2 3 5 6 7)))
 
-;;(define (union-set set1 set2)
+(define (union-set set1 set2) 
+   (cond  ((null? set1) set2) 
+          ((null? set2) set1) 
+          ((= (car set1) (car set2))  
+           (cons (car set1) (union-set (cdr set1) (cdr set2)))) 
+          ((< (car set1) (car set2))   
+           (cons (car set1) (union-set (cdr set1) set2))) 
+          (else  
+           (cons (car set2) (union-set set1 (cdr set2)))))) 
+  
